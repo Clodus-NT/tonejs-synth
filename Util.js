55f5513db324d4f4b023a49a1ffc35fae7822a0f
Clodus-NT <clodus.nt@gmail.com>
2022-10-23 15:46:56 -0400

    just doing some testing, but added MidiValues file that creates midi array

--- Util.js
+++ Util.js
@@ -3,6 +3,15 @@ function audioCtxReady() {
     background('black');
     fill('white');
     textAlign(CENTER, CENTER);
-    text('CLICK TO START', width/2, height/2)
+    text('CLICK TO START', width/2, height/2);
   }
+}
+
+function initAudio() {
+  mainOsc = new Tone.Oscillator();
+  mainOsc.type = 'square';
+  mainOsc.frequency.value = 220;
+  mainOsc.start();
+
+  mainOsc.toDestination(); // Connect to audio output
 }
--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 let ready = false;
-let osc;
-
+let mainOsc;
+// *******************************************************
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
-
+// *******************************************************
 function draw() {
   background(25);
   // fill('white');
@@ -12,13 +12,14 @@ function draw() {
   // text("CLICK TO START", width/2, height/2)
   audioCtxReady();
 }
-
+// *******************************************************
 function mousePressed() {
   if (ready === false) {
     ready = true;
+    initAudio();
   }
 }
-
+// *******************************************************
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
